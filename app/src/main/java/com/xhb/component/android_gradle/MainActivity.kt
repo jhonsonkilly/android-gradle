53f20732df49297994772467852e92bc38dabac6
zhoujiawei <1245145380@qq.com>
2020-12-16 10:48:25 +0800

    Initial commit

---
+++ app/src/main/java/com/xhb/component/android_gradle/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.xhb.component.android_gradle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
